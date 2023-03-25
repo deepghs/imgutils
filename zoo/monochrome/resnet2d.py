42dda6fac2a9ed72416bfb5eda13cb4a9bfa34ba
narugo1992 <narugo@126.com>
2023-03-25 22:33:40 +0800

    dev(narugo): update some format

--- zoo/monochrome/resnet2d.py
+++ zoo/monochrome/resnet2d.py
@@ -59,7 +59,7 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=2, avgpool_size: Tuple[int, int] = (16, 16)):
+    def __init__(self, block, num_blocks, num_classes=2, avgpool_size: Tuple[int, int] = (4, 4)):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
