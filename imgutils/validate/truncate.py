afcd56f06afc1e321a420ac43788803c6f997e73
narugo1992 <narugo@126.com>
2023-03-25 12:52:46 +0800

    dev(narugo): add full documentations for is_greyscale and is_truncated_file

--- imgutils/validate/truncate.py
+++ imgutils/validate/truncate.py
@@ -23,6 +23,33 @@ def _mock_load_truncated_images(value: bool):
 
 @_mock_load_truncated_images(False)
 def is_truncated_file(path: str) -> bool:
+    """
+    Overview:
+        Check if an image is truncated or not.
+
+    :param path: Path of file (must be a string which represents the path of image).
+    :return: Is truncated or not.
+
+    Examples:
+        Here are some images for example
+
+        .. figure:: truncated.dat.svg
+           :class: with-border
+           :align: center
+
+           Example images of truncated
+
+        >>> from imgutils.validate import is_truncated_file
+        >>>
+        >>> is_truncated_file('jpeg_full.jpeg')
+        False
+        >>> is_truncated_file('jpeg_truncated.jpeg')
+        True
+        >>> is_truncated_file('6125785.png')
+        False
+        >>> is_truncated_file('2216614_truncated.jpg')
+        True
+    """
     try:
         Image.open(path).load()
     except OSError:
