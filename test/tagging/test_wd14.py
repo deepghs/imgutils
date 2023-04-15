f018188de53de4f3536d5332bafaf4676cf7cc06
narugo1992 <narugo@126.com>
2023-04-15 10:33:22 +0800

    dev(narugo): add wd14 tagging

---
+++ test/tagging/test_wd14.py
@@ -0,0 +1,19 @@
+import pytest
+
+from imgutils.tagging import get_wd14_tags
+from test.testings import get_testfile
+
+
+@pytest.mark.unittest
+class TestTaggingWd14:
+    def test_get_wd14_tags(self):
+        rating, tags, chars = get_wd14_tags(get_testfile('6124220.jpg'))
+        assert rating['general'] > 0.9
+        assert tags['cat'] >= 0.8
+        assert not chars
+
+        rating, tags, chars = get_wd14_tags(get_testfile('6125785.jpg'))
+        assert 0.55 <= rating['general'] <= 0.65
+        assert 0.35 <= rating['sensitive'] <= 0.45
+        assert tags['1girl'] >= 0.95
+        assert chars['hu_tao_(genshin_impact)'] >= 0.95
