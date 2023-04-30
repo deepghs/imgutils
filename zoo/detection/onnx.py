de58de0dd5716676a16213d16424d3cc7d3bcf5c
narugo1992 <narugo@126.com>
2023-04-30 19:33:19 +0800

    dev(narugo): add manbits training script

--- zoo/detection/onnx.py
+++ zoo/detection/onnx.py
@@ -8,7 +8,7 @@ def export_yolo_to_onnx(yolo: YOLO, onnx_filename, opset_version: int = 14,
                         no_optimize: bool = False):
     if os.path.dirname(onnx_filename):
         os.makedirs(os.path.dirname(onnx_filename), exist_ok=True)
-    copy(
-        yolo.export(format='onnx', dynamic=True, simplify=not no_optimize, opset=opset_version),
-        onnx_filename
-    )
+
+    _retval = yolo.export(format='onnx', dynamic=True, simplify=not no_optimize, opset=opset_version)
+    _exported_onnx_file = _retval or (os.path.splitext(yolo.ckpt_path)[0] + '.onnx')
+    copy(_exported_onnx_file, onnx_filename)
