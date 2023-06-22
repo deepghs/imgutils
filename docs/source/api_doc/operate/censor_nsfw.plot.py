04de3f7b8533438583edc131a450d559c7c10b28
narugo1992 <narugo@126.com>
2023-06-22 23:40:35 +0800

    dev(narugo): optimize radius for pixelate method

--- docs/source/api_doc/operate/censor_nsfw.plot.py
+++ docs/source/api_doc/operate/censor_nsfw.plot.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     image_plot(
         (image, 'origin'),
         (censor_nsfw(image, 'color', nipple_f=True, color='black'), 'color (black)'),
-        (censor_nsfw(image, 'pixelate', nipple_f=True), 'pixelate'),
+        (censor_nsfw(image, 'pixelate', nipple_f=True, radius=12), 'pixelate'),
         (censor_nsfw(image, 'emoji', nipple_f=True), 'emoji'),
         columns=2,
         figsize=(7, 8),
