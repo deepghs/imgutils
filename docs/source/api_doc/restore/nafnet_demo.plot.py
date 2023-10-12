7a7cf362d2471ae7ef4f6e3cb5c7ade3f4b90ebb
narugo1992 <narugo@126.com>
2023-10-12 14:45:54 +0800

    dev(narugo): optimize image

--- docs/source/api_doc/restore/nafnet_demo.plot.py
+++ docs/source/api_doc/restore/nafnet_demo.plot.py
@@ -9,17 +9,22 @@ nafnet_dir = 'nafnet'
 
 if __name__ == '__main__':
     models: List[NafNetModelTyping] = ['REDS', 'GoPro', 'SIDD']
-    demo_images = os.listdir(sample_dir)
+    demo_images = [
+        ('original.png', 'Original'),
+        ('blur.png', 'Blur'),
+        ('jpg-q45.jpg', 'JPEG Quality45'),
+        ('jpg-q35.jpg', 'JPEG Quality35'),
+    ]
 
     items = []
-    for file in demo_images:
-        items.append((os.path.join(sample_dir, file), file))
+    for file, title in demo_images:
+        items.append((os.path.join(sample_dir, file), title))
         for model in models:
             dst_file = os.path.join(nafnet_dir, f'{os.path.splitext(file)[0]}-{model.lower()}.png')
-            items.append((dst_file, os.path.basename(dst_file)))
+            items.append((dst_file, f'{title}\n(Fixed By {model})'))
 
     image_plot(
         *items,
         columns=len(models) + 1,
-        figsize=(14, 7),
+        figsize=(12, 9),
     )
