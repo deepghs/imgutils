9aadf7895e430de5b75faa6e869d19e5c1652935
narugo1992 <narugo992@gmail.com>
2023-12-16 18:48:13 +0800

    dev(narugo): fix typo in doc scripts

--- docs/source/api_doc/validate/real.plot.py
+++ docs/source/api_doc/validate/real.plot.py
@@ -7,7 +7,7 @@ from plot import image_plot
 
 if __name__ == '__main__':
     image_plot(
-        *natsorted(glob.glob(os.path.join('nsfw', 'anime', '*.jpg'))),
-        *natsorted(glob.glob(os.path.join('nsfw', 'real', '*.jpg'))),
+        *natsorted(glob.glob(os.path.join('real', 'anime', '*.jpg'))),
+        *natsorted(glob.glob(os.path.join('real', 'real', '*.jpg'))),
         columns=4, figsize=(10, 15),
     )
