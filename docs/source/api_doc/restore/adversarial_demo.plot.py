367750e46d32ed5fc6666a0a102173b18f8b435e
narugo1992 <narugo992@gmail.com>
2023-12-18 16:18:18 +0800

    dev(narugo): fix the bug in plot

--- docs/source/api_doc/restore/adversarial_demo.plot.py
+++ docs/source/api_doc/restore/adversarial_demo.plot.py
@@ -9,10 +9,8 @@ sample_dir = 'sample'
 if __name__ == '__main__':
     image = load_image(os.path.join(sample_dir, 'adversarial_input.png'))
     image_plot(
-        [
-            (image, 'Adversarial Noised'),
-            (remove_adversarial_noise(image), 'Cleaned')
-        ],
+        (image, 'Adversarial Noised'),
+        (remove_adversarial_noise(image), 'Cleaned'),
         columns=2,
         figsize=(10, 6),
     )
