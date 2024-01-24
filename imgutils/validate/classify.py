27adaabfd649628ee8101d55135ed423772c573a
narugo1992 <narugo992@gmail.com>
2024-01-24 16:06:13 +0800

    dev(narugo): update classify

--- imgutils/validate/classify.py
+++ imgutils/validate/classify.py
@@ -15,62 +15,18 @@ Overview:
     The models are hosted on
     `huggingface - deepghs/anime_classification <https://huggingface.co/deepghs/anime_classification>`_.
 """
-from functools import lru_cache
-from typing import Tuple, Optional, Dict
+from typing import Tuple, Dict
 
-import numpy as np
-from PIL import Image
-from huggingface_hub import hf_hub_download
-
-from imgutils.data import rgb_encode, ImageTyping, load_image
-from imgutils.utils import open_onnx_model
+from ..data import ImageTyping
+from ..generic import classify_predict, classify_predict_score
 
 __all__ = [
     'anime_classify_score',
     'anime_classify',
 ]
 
-_LABELS = ['3d', 'bangumi', 'comic', 'illustration']
-_MODEL_NAMES = [
-    'caformer_s36',
-    'caformer_s36_plus',
-    'mobilenetv3',
-    'mobilenetv3_dist',
-    'mobilenetv3_sce',
-    'mobilenetv3_sce_dist',
-    'mobilevitv2_150',
-]
 _DEFAULT_MODEL_NAME = 'mobilenetv3_sce_dist'
-
-
-@lru_cache()
-def _open_anime_classify_model(model_name):
-    return open_onnx_model(hf_hub_download(
-        f'deepghs/anime_classification',
-        f'{model_name}/model.onnx',
-    ))
-
-
-def _img_encode(image: Image.Image, size: Tuple[int, int] = (384, 384),
-                normalize: Optional[Tuple[float, float]] = (0.5, 0.5)):
-    image = image.resize(size, Image.BILINEAR)
-    data = rgb_encode(image, order_='CHW')
-
-    if normalize is not None:
-        mean_, std_ = normalize
-        mean = np.asarray([mean_]).reshape((-1, 1, 1))
-        std = np.asarray([std_]).reshape((-1, 1, 1))
-        data = (data - mean) / std
-
-    return data.astype(np.float32)
-
-
-def _raw_anime_classify(image: ImageTyping, model_name: str = _DEFAULT_MODEL_NAME):
-    image = load_image(image, force_background='white', mode='RGB')
-    input_ = _img_encode(image)[None, ...]
-    output, = _open_anime_classify_model(model_name).run(['output'], {'input': input_})
-
-    return output
+_REPO_ID = 'deepghs/anime_classification'
 
 
 def anime_classify_score(image: ImageTyping, model_name: str = _DEFAULT_MODEL_NAME) -> Dict[str, float]:
@@ -111,9 +67,7 @@ def anime_classify_score(image: ImageTyping, model_name: str = _DEFAULT_MODEL_NA
         >>> anime_classify_score('classify/illustration/12.jpg')
         {'3d': 3.153582292725332e-05, 'bangumi': 0.0001071861624950543, 'comic': 5.665345452143811e-05, 'illustration': 0.999804675579071}
     """
-    output = _raw_anime_classify(image, model_name)
-    values = dict(zip(_LABELS, map(lambda x: x.item(), output[0])))
-    return values
+    return classify_predict_score(image, _REPO_ID, model_name)
 
 
 def anime_classify(image: ImageTyping, model_name: str = _DEFAULT_MODEL_NAME) -> Tuple[str, float]:
@@ -154,6 +108,4 @@ def anime_classify(image: ImageTyping, model_name: str = _DEFAULT_MODEL_NAME) ->
         >>> anime_classify('classify/illustration/12.jpg')
         ('illustration', 0.999804675579071)
     """
-    output = _raw_anime_classify(image, model_name)[0]
-    max_id = np.argmax(output)
-    return _LABELS[max_id], output[max_id].item()
+    return classify_predict(image, _REPO_ID, model_name)
