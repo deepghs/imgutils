c7d6bb6e73f6ce41983c64034480e4316452c8c3
narugo1992 <narugo992@gmail.com>
2024-03-25 17:01:21 +0800

    dev(narugo): update full overview

--- docs/source/api_doc/metrics/dbaesthetic_full.plot.py
+++ docs/source/api_doc/metrics/dbaesthetic_full.plot.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     for label in _LABELS:
         image = os.path.join('dbaesthetic', f'{label}.jpg')
         l, p, s = anime_dbaesthetic(image, fmt=('label', 'percentile', 'score'))
-        display_name = f'l\nScore: {p:.2f}/{6.0:.2f}\nPercentile: {s:.3f}'
+        display_name = f'{l}\nScore: {s:.2f}/{6.0:.2f}\nPercentile: {p:.3f}'
         items.append((image, display_name))
 
     image_plot(
