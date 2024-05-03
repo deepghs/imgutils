f3ce0ad8daa2c2a06f461fb1cf67d594b42ed310
narugo1992 <narugo992@gmail.com>
2024-05-03 14:59:42 +0800

    dev(narugo): add unittest and docs

---
+++ docs/source/api_doc/metrics/laplacian.rst
@@ -0,0 +1,15 @@
+imgutils.metrics.laplacian
+=================================
+
+.. currentmodule:: imgutils.metrics.laplacian
+
+.. automodule:: imgutils.metrics.laplacian
+
+
+laplacian_score
+--------------------------------------
+
+.. autofunction:: laplacian_score
+
+
+
---
+++ docs/source/api_doc/metrics/laplacian_full.plot.py
@@ -0,0 +1,18 @@
+import os.path
+
+from imgutils.metrics import laplacian_score
+from plot import image_plot
+
+if __name__ == '__main__':
+    items = []
+    for filename in os.listdir('laplacian'):
+        image = os.path.join('laplacian', filename)
+        ls = laplacian_score(image)
+        display_name = f'{filename}\nScore: {ls:.3f}'
+        items.append((image, display_name))
+
+    image_plot(
+        *items,
+        columns=2,
+        figsize=(6, 5),
+    )
