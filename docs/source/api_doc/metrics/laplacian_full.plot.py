4499dd75e999de8e1f5142fb648146da9051b4bc
narugo1992 <narugo992@gmail.com>
2024-05-03 15:00:12 +0800

    dev(narugo): add unittest and docs

--- docs/source/api_doc/metrics/laplacian_full.plot.py
+++ docs/source/api_doc/metrics/laplacian_full.plot.py
@@ -1,11 +1,13 @@
 import os.path
 
+from natsort import natsorted
+
 from imgutils.metrics import laplacian_score
 from plot import image_plot
 
 if __name__ == '__main__':
     items = []
-    for filename in os.listdir('laplacian'):
+    for filename in natsorted(os.listdir('laplacian')):
         image = os.path.join('laplacian', filename)
         ls = laplacian_score(image)
         display_name = f'{filename}\nScore: {ls:.3f}'
