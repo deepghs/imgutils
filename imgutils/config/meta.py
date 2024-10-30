d39b451335b229af337cc32a69a838ea01c4e14e
narugo1992 <narugo992@gmail.com>
2024-10-30 19:25:05 +0800

    dev(narugo): use version v0.6.1

--- imgutils/config/meta.py
+++ imgutils/config/meta.py
@@ -7,7 +7,7 @@ Overview:
 __TITLE__ = 'imgutils'
 
 #: Version of this project.
-__VERSION__ = '0.6.0'
+__VERSION__ = '0.6.1'
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = 'A convenient and user-friendly anime-style image data processing library that integrates ' \
