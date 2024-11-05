00d8b0e2a65de9dd3487f80c80ca7867f394a527
narugo1992 <narugo992@gmail.com>
2024-11-05 23:15:23 +0800

    dev(narugo): add furry unittest and benchmark

---
+++ docs/source/api_doc/validate/furry.plot.py
@@ -0,0 +1,13 @@
+import glob
+import os.path
+
+from natsort import natsorted
+
+from plot import image_plot
+
+if __name__ == '__main__':
+    image_plot(
+        *natsorted(glob.glob(os.path.join('furry', 'non_furry', '*.jpg'))),
+        *natsorted(glob.glob(os.path.join('furry', 'furry', '*.jpg'))),
+        columns=4, figsize=(10, 15),
+    )
