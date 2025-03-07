8c26c2a5af95f9630fa0dd921cbb669a0f72f3a3
narugo1992 <narugo992@gmail.com>
2025-03-07 19:42:09 +0800

    dev(narugo): add cross test

--- test/data/test_blob.py
+++ test/data/test_blob.py
@@ -1,10 +1,32 @@
 import pytest
+from hbutils.testing import tmatrix
 
-from imgutils.data import is_valid_image_blob_url
+from imgutils.data import is_valid_image_blob_url, load_image, to_blob_url, load_image_from_blob_url
+from test.testings import get_testfile
 
 
 @pytest.mark.unittest
 class TestDataBlob:
+    @pytest.mark.parametrize(*tmatrix({
+        'filename': [
+            'mostima_post.jpg',
+            'soldiers.jpg',
+            'nian.png',
+        ],
+        'format': [
+            'jpg',
+            'png',
+            'webp',
+        ]
+    }, mode='matrix'))
+    def test_to_blob_url_and_load_image_from_blob_url(self, filename, format, image_diff):
+        original_image = load_image(get_testfile(filename), mode='RGB', force_background='white')
+        assert image_diff(
+            original_image,
+            load_image_from_blob_url(to_blob_url(original_image, format=format)),
+            throw_exception=False,
+        ) < 1.5e-2
+
     @pytest.mark.parametrize(['blob_url', 'expected_result'], [
         ("data:image/png;base64,ABC", True),
         ("DATA:IMAGE/JPEG,", True),
